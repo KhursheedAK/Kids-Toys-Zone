63875cd1c8487071e92f9c25c212afdec2948f36
KhursheedAK <143898884+KhursheedAK@users.noreply.github.com>
2024-12-04 17:31:04 +0500

    kid toys react app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,112 @@
-import logo from './logo.svg';
-import './App.css';
+import toyData from './toyData';
+import './index.css';
+/* Getting Data from Toy Data File */
+const data = toyData();
+
+function Header() {
+  return (
+    <header>
+      <h1>Toy Store</h1>
+    </header>
+  );
+}
+
+function ShowcaseToys() {
+  const dataSize = data.length;
+
+  return (
+    <main>
+      <div className="toy-logo">
+        <img
+          src="/logo192.avif"
+          alt="toy-logo"
+          style={{ width: '192px', borderRadius: '20px' }}
+        />
+        <div className="freepik">
+          <a href="http://www.freepik.com">Toy Shop Illustration - Freepik</a>
+        </div>
+      </div>
+      <h2 className="collection-heading-wrapper">
+        <div className="collection-heading">Toy Collection for Kids</div>
+      </h2>
+      <div>
+        <ul className="showcaseToys">
+          {dataSize > 0 ? (
+            data.map((toys) => <ListToys toysObjects={toys} key={toys.id} />)
+          ) : (
+            <p> No Toys Available Right Now! Check back later</p>
+          )}
+        </ul>
+      </div>
+    </main>
+  );
+}
+
+function ListToys({ toysObjects }) {
+  return (
+    <li className={`toysObjects ${toysObjects.soldOut ? 'sold-out' : ''}`}>
+      <h3>{toysObjects.title}</h3>
+      <img className="image" src={toysObjects.image} alt={toysObjects.title} />
+      <div className="toysTraits">
+        <p>
+          <strong> Manufactured Date: </strong>
+          {toysObjects.manufactered}
+        </p>
+        <p>
+          <strong>Description: </strong>
+          <span>{toysObjects.description}</span>
+        </p>
+        <p>
+          <strong>Price: </strong>${toysObjects.price}
+        </p>
+        <p>
+          <strong>Status: </strong>{' '}
+          {toysObjects.soldOut ? 'Sold Out' : 'Available'}
+        </p>
+      </div>
+    </li>
+  );
+}
+
+function Footer() {
+  const hours = new Date().getHours();
+  const openingHours = 9;
+  const closingHours = 17;
+  const isOpen = hours >= openingHours && hours <= closingHours;
+
+  return (
+    <>
+      <footer>
+        {isOpen ? (
+          <ShopTimings closingHour={closingHours} openingHours={openingHours} />
+        ) : (
+          <p>
+            Physical Shop is closed until {openingHours}:00 A.M. but online
+            purchases are available 24/7
+          </p>
+        )}
+      </footer>
+    </>
+  );
+}
+
+function ShopTimings({ closingHour, openingHours }) {
+  return (
+    <div>
+      <p>
+        Physical Shop is open from {openingHours}:00 A.M. to {closingHour}:00
+        P.M.
+      </p>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Header />
+      <ShowcaseToys />
+      <Footer />
     </div>
   );
 }
---
+++ src/toyData.js
@@ -0,0 +1,95 @@
+export default function toyData() {
+  const data = [
+    {
+      id: 1,
+      title: 'buzz light year balloon',
+      manufactered: '10-02-2012',
+      description:
+        'A helium-filled balloon featuring Buzz Lightyear from the iconic Toy Story movie.',
+      price: 12,
+      soldOut: false,
+      image: '/assets/buzzlightyear balloon.avif',
+    },
+    {
+      id: 2,
+      title: 'lego figure',
+      manufactered: '12-04-2015',
+      description:
+        'A small, customizable Lego toy figure with movable parts, perfect for creative play and display.',
+      price: 29,
+      soldOut: false,
+      image: '/assets/lego figure.avif',
+    },
+    {
+      id: 3,
+      title: 'lego house',
+      manufactered: '21-06-2017',
+      description:
+        'A colorful Lego house toy designed for creative building and imaginative play.',
+      price: 34,
+      soldOut: false,
+      image: '/assets/lego house.avif',
+    },
+    {
+      id: 4,
+      title: 'lego train',
+      manufactered: '16-08-2018',
+      description:
+        'A detailed Lego train toy perfect for building, play, and display.',
+      price: 50,
+      soldOut: false,
+      image: '/assets/lego train.avif',
+    },
+    {
+      id: 5,
+      title: 'marvel collection',
+      manufactered: '12-01-2019',
+      description:
+        'A Marvel-inspired Lego collection featuring iconic Batman and Superman figures.',
+      price: 70,
+      soldOut: true,
+      image: '/assets/marvel collection.avif',
+    },
+    {
+      id: 6,
+      title: 'pink jeep',
+      manufactered: '16-08-2020',
+      description:
+        'A battery-powered drivable pink jeep for kids, combining fun and adventure.',
+      price: 299,
+      soldOut: false,
+      image: '/assets/pink jeep.avif',
+    },
+    {
+      id: 7,
+      title: 'pokemon cards',
+      manufactered: '07-01-2013',
+      description:
+        'A collectible set of Pokémon cards featuring vibrant designs and iconic characters.',
+      price: 40,
+      soldOut: false,
+      image: '/assets/pokemon cards.avif',
+    },
+    {
+      id: 8,
+      title: 'pokemon patch',
+      manufactered: '11-03-2014',
+      description:
+        'An embroidered Pokémon patch showcasing vibrant designs of iconic characters.',
+      price: 20,
+      soldOut: false,
+      image: '/assets/pokemon patch.avif',
+    },
+    {
+      id: 9,
+      title: 'yellow cars',
+      manufactered: '26-03-2022',
+      description:
+        'A set of vibrant yellow toy cars, perfect for play and collection.',
+      price: 50,
+      soldOut: false,
+      image: '/assets/yellow cars.avif',
+    },
+  ];
+  return data;
+}
